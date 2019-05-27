16374997fb26f3bff0d145e6826ce78c71094991
loganseals <loganseals@users.noreply.github.com>
2019-05-27 13:28:04 -0500

    Update Coaster_Features.js

--- Coaster_Features.js
+++ Coaster_Features.js
@@ -4,13 +4,18 @@ const FLIP = 'http://flip3.engr.oregonstate.edu:';
 /* Constant for port number - can be changed to test on a different port */
 const PORT = '9896';
 
-// define the ID for the page upon loading
+
 const urlParams = new URLSearchParams(window.location.search);
-const ID = 1;
-// const ID = urlParams.get('id');
+const ID = urlParams.get('id');
+
 
 // function to load the table when the page has loaded
 document.addEventListener("DOMContentLoaded", pageSetup(ID) );
+
+// function to load the table when the page has loaded
+document.addEventListener("DOMContentLoaded", loadBasicFeatures(ID) );
+
+
 
 // function to add functionality to the "Add Feature" button
 document.getElementById('addfeature').addEventListener('click', function(event) {
@@ -28,6 +33,42 @@ function pageSetup(ID) {
 	addFeatures(ID);
 
 }
+
+/* function to load the coaster's basic features at the top of the page */
+function loadBasicFeatures(ID) {
+    /* Initialize Ajax Request */
+    let req = new XMLHttpRequest();
+    req.open("GET", FLIP + PORT + "/search-coasters-by-id?id=" + ID, true);
+
+    req.addEventListener("load", function addRetrievedData() {
+        /* If there was no error, prefill form with data. */
+        if (req.status >= 200 && req.status < 400) {
+            let res = JSON.parse(req.responseText)[0];
+
+            document.getElementById("coasterNameContent").innerHTML = "Name: " +  res.name;
+
+            /* If the park is not null, update the value of the "park" field with the correct park. */
+            if (res.park !== null) {
+                document.getElementById("coasterParkContent").innerHTML = "Park: " + res.park;
+            }
+
+       		document.getElementById("coasterManufacturerContent").innerHTML = "Manufacturer: " + res.manufacturer;
+
+            document.getElementById("coasterYearContent").innerHTML = "Year Opened: " + res.year_opened;
+
+            document.getElementById("coasterHeightContent").innerHTMl = "Height (ft): " + res.height;
+
+            document.getElementById("coasterSpeedContent").innerHTMl = "Max Speed (mph): " + res.max_speed;
+
+            document.getElementById("coasterOperationContent").innerHTML = "In Operation: " + res.in_operation;
+        }
+        else {
+            alert("An error occurred getting data from the server.");
+        }
+    });
+    req.send(null);
+}
+
 
 // delete function to remove feature and coaster relationship from the database
 function deleteEntry(feature, ID, event) {
